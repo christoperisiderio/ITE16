dbf226a7ac7bb8a3cae8f8b954876b7ca8d8cf28
christoperisiderio <christoperisiderio@gmail.com>
2025-02-15 19:50:28 +0800

    main

--- dvdrental/dvdrental.sql
+++ dvdrental/dvdrental.sql
@@ -1,5 +1,5 @@
 
--- 1. 
+-- 1. Action-packed romance
 SELECT 
     film.title, 
     category.name AS category_name, 
@@ -22,7 +22,7 @@ ORDER BY
     rental_count DESC
 LIMIT 5;
 
--- 2.
+-- 2.Classics for couples
 SELECT *
 FROM (
     SELECT 
@@ -47,6 +47,29 @@ FROM (
 ) subquery
 WHERE 
     day = 14 AND month = 2 AND year < 2007
+ORDER BY 
+    rental_count DESC
+LIMIT 3;
+
+-- 3. Family-friendly fun
+SELECT 
+    film.title, 
+    category.name AS category_name, 
+    COUNT(rental.rental_id) AS rental_count
+FROM 
+    film
+INNER JOIN 
+    film_category ON film.film_id = film_category.film_id
+INNER JOIN 
+    category ON film_category.category_id = category.category_id
+INNER JOIN 
+    inventory ON film.film_id = inventory.film_id
+INNER JOIN 
+    rental ON inventory.inventory_id = rental.inventory_id
+WHERE 
+     category.name='Children' OR category.name='Animation'
+GROUP BY 
+    film.title, category.name
 ORDER BY 
     rental_count DESC
 LIMIT 5;
