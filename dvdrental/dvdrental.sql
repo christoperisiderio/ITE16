c4fcb9c127c20a4085323d5f4a2bb0609100b377
christoperisiderio <christoperisiderio@gmail.com>
2025-02-15 19:34:10 +0800

    main

--- dvdrental/dvdrental.sql
+++ dvdrental/dvdrental.sql
@@ -1,3 +1,5 @@
+
+-- 1. 
 SELECT 
     film.title, 
     category.name AS category_name, 
@@ -16,6 +18,35 @@ WHERE
     category.name = 'Action'
 GROUP BY 
     film.title, category.name
+ORDER BY 
+    rental_count DESC
+LIMIT 5;
+
+-- 2.
+SELECT *
+FROM (
+    SELECT 
+        film.title, 
+        category.name AS category_name, 
+        COUNT(rental.rental_id) AS rental_count,
+        EXTRACT(YEAR FROM rental.rental_date) as year,
+        EXTRACT(MONTH FROM rental.rental_date) as month,
+        EXTRACT(DAY FROM rental.rental_date) as day
+    FROM 
+        film
+    INNER JOIN 
+        film_category ON film.film_id = film_category.film_id
+    INNER JOIN 
+        category ON film_category.category_id = category.category_id
+    INNER JOIN 
+        inventory ON film.film_id = inventory.film_id
+    INNER JOIN 
+        rental ON inventory.inventory_id = rental.inventory_id
+    GROUP BY 
+        film.title, category.name, year, month, day
+) subquery
+WHERE 
+    day = 14 AND month = 2 AND year < 2007
 ORDER BY 
     rental_count DESC
 LIMIT 5;
